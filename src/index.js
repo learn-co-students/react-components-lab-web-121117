3ba68138199d47f282ad4f342ac6a4cbb0ccc948
github username <github email address>
2018-02-16 16:41:01 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -39,11 +39,6 @@ export class ButcherShop extends React.Component {
   }
 }
 
-// Define these exported classes
-// export class OlderCoaster extends React.Component {}
-// export class InFrontOfYou extends React.Component {}
-// export class ButcherShop extends React.Component {}
-
 ReactDOM.render(
   React.createElement("div", {}, [
     React.createElement(OlderCoaster),
